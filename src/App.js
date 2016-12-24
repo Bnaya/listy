16bbf1f605b71463deaa7dda936a897c2d7f9066
nitz <nitz.raz@gmail.com>
2016-12-24 13:02:31 +0200

    Migrated to bootstrap look. Table looks a lot better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ReactDataGrid from 'react-data-grid';
 import {table_data} from './Table.js';
 import 'react-data-grid/dist/react-data-grid.css';
@@ -8,14 +7,8 @@ import 'react-data-grid/dist/react-data-grid.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React15</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h2>Welcome to React5</h2>
         <ReactDataGrid
         columns={table_data.columns}
         rowGetter={table_data.rowGetter}
